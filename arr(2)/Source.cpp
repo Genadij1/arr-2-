1d6e053a610929f4017f454f34ff2c84ad22bddd
Геннадий <@DESKTOP-6NN51IE>
2023-05-31 12:47:59 +0300

    ex_2-done

--- arr(2)/Source.cpp
+++ arr(2)/Source.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main() {
-	
+
 	const int ROW = 5;
 	const int COL = 5;
 	int arr[ROW][COL]{
@@ -13,17 +13,32 @@ int main() {
 		{4,4,4,4,4},
 		{5,5,5,5,5}
 	};
-	for (int i = 0; i < ROW; i++) {
-		int sum1 = 0;
-		int sum2 = 0;
-		for (int k = 0; k < COL; k++) {
+
+	int i = 0;
+	int sum1;
+	int sm;
+	for (int i=0; i < ROW; i++) {
+		sum1 = 0;
+		sm = 0;
+		for (int k=0; k < COL; k++) {
 			cout << arr[i][k] << "\t";
 			sum1 += arr[i][k];
-			sum2 += arr[k][i];
-			cout << sum2;
+			sm += sum1;
+
 		}
-		cout <<sum1<< endl;
+		cout << sum1 << endl;
 	}
+	for (int i=0; i < COL; i++) {
+		int sum2 = 0;
+		for (int k = 0; k < ROW; k++) {
+			sum2 += arr[k][i];
+		}
+		cout << sum2 << "\t";
+		if (i+1 == COL) {
+			cout << sm;
+		}
+	}
+
 
 	system("pause>nul");
 	return 0;
